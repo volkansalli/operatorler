9536a2103dd8a390c60b599b96910fb735b5ebf2
volkan salli <volkan_salli@hotmail.com>
2021-11-08 23:08:23 +0300

    operatorodevi

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        //Atama ve İşlemli Atama Operatörleri (=, +=, -=, *=, /=)
+        int i=0;
+        i+=1;
+        Console.WriteLine(i);
+
+        //Mantıksal Operatörler (||, &&, !)
+        //İlişkisel Operatörler (==,!=, <, >, >=,<=)
+        int x=0;
+        if(x!=1)
+        Console.WriteLine(x);
+
+        
+        //Aritmetik (+, -, *, /, %, ++, --)
+        x++;
+        Console.WriteLine(x);
+        }
+    }
+}
